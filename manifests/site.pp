067f16a3ae1f6cb7b989e7f7e958fd7e93d1e871
Charles Benson <34258017+CMBENSON@users.noreply.github.com>
2022-03-25 11:25:55 +0000

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,5 +1,17 @@
 node default {
+  file {'/root/README':
+    ensure => file,
+    content => 'This is a readme',
+    owner => 'root',
 }
 node 'master.puppet.vm' {
   include role::master_server 
 }
+
+node /^web/ {
+  incdue role::app_server
+}
+
+node /^db/ {
+  incdue role::dv_server
+} 
